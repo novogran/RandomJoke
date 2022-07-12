354f0e5fd0f6352b5b0f661377e54e2d0ce0dc85
Vitali Novogran <vi19954@gmail.com>
2022-07-12 11:36:49 +0300

    Fix progressbar

--- app/src/main/java/com/example/randomjoke/presentation/MainActivity.kt
+++ app/src/main/java/com/example/randomjoke/presentation/MainActivity.kt
@@ -11,14 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val progressBar = findViewById<CorrectProgress>(R.id.progressBar)
-        progressBar.visibility = View.INVISIBLE
         val viewModel = (application as JokeApp).viewModel
         val favoriteDataView = findViewById<FavoriteDataView>(R.id.jokeFavoriteDataView)
         favoriteDataView.linkWith(viewModel)
         viewModel.observe(this) { state ->
            favoriteDataView.show(state)
         }
+
         val quoteViewModel = (application as JokeApp).quoteViewModel
         val quoteFavoriteDataView = findViewById<FavoriteDataView>(R.id.quoteFavoriteDataView)
         quoteFavoriteDataView.linkWith(quoteViewModel)
